9b67aeb926cffba513e964c262d16a14891c3181
George Phillips <george@lameco.co.za>
2016-06-14 15:48:28 +0200

    images

---
+++ scripts/services/FacebookService.ts
@@ -0,0 +1,12 @@
+app.service('FacebookService',
+  function ($http, config, localStorageService, $q) {
+  var base = config.baseServiceURL + "/v1/";
+  var profileKey = "profileKey";
+
+  return {
+    login: function () {
+
+    }
+  };
+}
+);
