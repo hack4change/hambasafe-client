2e176b94f7dcc6d49da6b5537ff40833e9edd5ac
Jacobus Langner <madangryscientist@gmail.com>
2016-06-22 23:23:41 +0200

    more clean up

--- app/chats/ChatDetailCtrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var ChatDetailComponent = (function () {
-    function ChatDetailComponent($stateParams, chatService) {
-        this.chat = chatService.get($stateParams.chatId);
-    }
-    return ChatDetailComponent;
-}());
-exports.ChatDetailComponent = ChatDetailComponent;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ2hhdERldGFpbEN0cmwuanMiLCJzb3VyY2VSb290IjoiQzovaGFjazRjaGFuZ2UvaGFtYmFzYWZlLWNsaWVudC9hcHAvIiwic291cmNlcyI6WyJjaGF0cy9DaGF0RGV0YWlsQ3RybC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBRUE7SUFHRSw2QkFBWSxZQUFZLEVBQUUsV0FBd0I7UUFDaEQsSUFBSSxDQUFDLElBQUksR0FBRyxXQUFXLENBQUMsR0FBRyxDQUFDLFlBQVksQ0FBQyxNQUFNLENBQUMsQ0FBQztJQUNuRCxDQUFDO0lBQ0gsMEJBQUM7QUFBRCxDQUFDLEFBTkQsSUFNQztBQU5ZLDJCQUFtQixzQkFNL0IsQ0FBQSJ9
--- app/services/LocalStorage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var LocalStorage = (function () {
-    function LocalStorage() {
-        this.facebookAuth = null;
-    }
-    return LocalStorage;
-}());
-exports.LocalStorage = LocalStorage;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiTG9jYWxTdG9yYWdlLmpzIiwic291cmNlUm9vdCI6IkM6L2hhY2s0Y2hhbmdlL2hhbWJhc2FmZS1jbGllbnQvYXBwLyIsInNvdXJjZXMiOlsic2VydmljZXMvTG9jYWxTdG9yYWdlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQTtJQUVFO1FBREEsaUJBQVksR0FBRyxJQUFJLENBQUM7SUFHcEIsQ0FBQztJQUdILG1CQUFDO0FBQUQsQ0FBQyxBQVBELElBT0M7QUFQWSxvQkFBWSxlQU94QixDQUFBIn0=
